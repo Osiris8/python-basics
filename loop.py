b661c838c5b792e45d15eda57e9f25fa2f2a4c6f
Osiris8 <miganosiris8@gmaaail.com>
2025-05-11 18:47:32 +0200

    python loop

---
+++ loop.py
@@ -0,0 +1,38 @@
+numbers = [0, 254, 2, -1, 3]
+
+for num in numbers:
+  if (num < 0):
+    print("Negative number detected!")
+    break
+  print(num)
+  
+# 0
+# 254
+# 2
+# Negative number detected!
+
+
+big_number_list = [1, 2, -1, 4, -5, 5, 2, -9]
+
+# Print only positive numbers:
+for i in big_number_list:
+  if i < 0:
+    continue
+  print(i)
+
+  # Print the numbers 0, 1, 2:
+for i in range(3):
+  print(i)
+
+# Print "WARNING" 3 times:
+for i in range(3):
+  print("WARNING")
+
+
+groups = [["Jobs", "Gates"], ["Newton", "Euclid"], ["Einstein", "Feynman"]]
+
+# This outer loop will iterate over each list in the groups list
+for group in groups:
+  # This inner loop will go through each name in each list
+  for name in group:
+    print(name)
